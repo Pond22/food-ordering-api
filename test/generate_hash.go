562549a59f9a01107d308817f2126167bc4aa4f5
Pond22 <pondba22@gmail.com>
2025-02-17 20:57:36 +0700

    setup

---
+++ test/generate_hash.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	password := "123456"
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Printf("Error generating hash: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Password: %s\nHash: %s\n", password, string(hash))
+}
